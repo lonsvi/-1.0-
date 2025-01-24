3d09cf9df4bb825ea6f2de2061331af2a2c4d054
lonsvi <lonsvi@BOFFY>
2025-01-25 01:21:40 +0800

    Добавьте файлы проекта.

---
+++ Визуальная новелла (1.0)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Визуальная_новелла__1._0_
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
